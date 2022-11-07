62369b99d1eb92178ad78968dd24ef55e4da7f4f
MustafaBasar471 <mustafabasar471@gmail.com>
2022-11-08 01:33:15 +0300

    Starter

---
+++ src/context/Context.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const [data, setData] = useState(null);
+
+  return (
+    <Context.Provider
+      value={{
+        data,
+        setData,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
